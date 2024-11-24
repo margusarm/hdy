10a83a43379e3a3affa938c3153671b0a764a8db
margusarm <margus.arm@gmail.com>
2024-11-24 20:43:46 +0200

    paar updatet ja ilusam ikoon

--- src/App.tsx
+++ src/App.tsx
@@ -3,90 +3,107 @@ import React from 'react';
 import logo from './greta_soon_big.png'
 import './App.css';
 import Box from '@mui/material/Box';
-import { Button, FilledInput, FormControl, FormHelperText, Input, InputAdornment, OutlinedInput, Slider, Stack, Typography } from '@mui/material';
+import { Button, FilledInput, FormControl, FormHelperText, Grid2 as Grid, Input, InputAdornment, OutlinedInput, Slider, Stack, Typography } from '@mui/material';
+import StorageIcon from '@mui/icons-material/Storage';
 
 function App() {
 
 
   return (
-  
+
     <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
-    <body>
-      <Box
-        sx={{
-          height: '90vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          px: 6, // Padding for small screens
-        }}
-      >
-        <Stack
-          spacing={3} // Adds spacing between elements
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <body>
+        <Box
           sx={{
-            maxWidth: '600px',
-            width: '100%', // Full width on smaller screens
+            height: '90vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            px: 6, // Padding for small screens
           }}
         >
-          <FormControl
-            variant="standard"
+          <Stack
+            spacing={3} // Adds spacing between elements
             sx={{
-              m: 1,
-              width: '100%',
-              maxWidth: '200px', // Limit width on larger screens
-              alignSelf: 'center', // Center in the stack
+              maxWidth: '600px',
+              width: '100%', // Full width on smaller screens
             }}
           >
-                <Typography variant="subtitle1" gutterBottom>
-              Kui suur su kodu on?
-            </Typography>       
-            <Box
-            sx={{
-              maxWidth: 100,
-              alignSelf: 'center'
-            }}
-            >
-               
-            <Input
-              id="standard-adornment-weight"
-              endAdornment={<InputAdornment position="end">m2</InputAdornment>}
-              aria-describedby="standard-weight-helper-text"
-              inputProps={{
-                'aria-label': 'weight',
+            <FormControl
+              variant="standard"
+              sx={{
+                m: 1,
+                width: '100%',
+                maxWidth: '200px', // Limit width on larger screens
+                alignSelf: 'center', // Center in the stack
               }}
-            />
-            </Box>
-          </FormControl>
+            >
+              <Typography variant="subtitle1" gutterBottom>
+                Kui suur su kodu on?
+              </Typography>
+              <Box
+                sx={{
+                  maxWidth: 100,
+                  alignSelf: 'center'
+                }}
+              >
 
-          <Box sx={{ width: '100%', maxWidth: 400, mx: 'auto', alignSelf: 'center' }}>
-            <Typography variant="subtitle1" gutterBottom sx={{mb: 4}}>
-              Mis aastal su kodu ehitatud on?
-            </Typography>
-            <Slider 
-            min={1920} 
-            max={2020}
-            step={10} 
-            defaultValue={1960} 
-            aria-label="Default" 
-            valueLabelDisplay="on" 
-            marks
-            />
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography variant="body2">1920</Typography>
-              <Typography variant="body2">2020</Typography>
-            </Box>
-          </Box>
+                <Input
+                  type='number'
+                  id="pindala"
+                  endAdornment={<InputAdornment position="end">m2</InputAdornment>}
+                  aria-describedby="Sisesta pindala"
+                  inputProps={{
+                    'aria-label': 'pindala',
+                  }}
+                />
+              </Box>
+            </FormControl>
 
-          <Button variant="contained" sx={{ alignSelf: 'center', width: '50%' }}>
-            Arvuta
-          </Button>
-        </Stack>
-      </Box>
-    </body>
-  </div>
+            <Box sx={{ width: '100%', maxWidth: 400, mx: 'auto', alignSelf: 'center' }}>
+              <Typography variant="subtitle1" gutterBottom sx={{ mb: 4 }}>
+                Mis aastal su kodu ehitatud on?
+              </Typography>
+              <Slider
+                min={1920}
+                max={2020}
+                step={10}
+                defaultValue={1960}
+                aria-label="Default"
+                valueLabelDisplay="on"
+                marks
+              />
+              <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Typography variant="body2">1920</Typography>
+                <Typography variant="body2">2020</Typography>
+              </Box>
+            </Box>
+
+            <Button variant="contained" sx={{ alignSelf: 'center', width: '50%' }}>
+              Arvuta
+            </Button>
+            <Grid>
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+              <StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon /><StorageIcon />
+            </Grid>
+
+          </Stack>
+        </Box>
+      </body>
+    </div>
   );
 }
 
